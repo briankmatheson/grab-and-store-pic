c3c016ddf826c46dc628a53d9d2adabd89a08333
Brian Matheson <bmath@brianmatheson.com>
2023-01-12 05:13:39 -0500

    add supporting db code

---
+++ src/models.rs
@@ -0,0 +1,17 @@
+use crate::schema::messages;
+
+#[derive(Queryable, Serialize, Debug)]
+pub struct Message {
+    pub id: i32,
+    pub username: String,
+    pub message: String,
+    pub timestamp: i64,
+}
+
+#[derive(Insertable, Debug)]
+#[diesel(table_name = messages)]
+pub struct NewMessage {
+    pub username: String,
+    pub message: String,
+}
+
